3191202f54c7df1aca54e8b3f6f2bef01840ab95
KVarad777 <khv9058@gmail.com>
2026-08-21 06:56:14 +0530

    chore: initialize Spring Boot result application

---
+++ Assignment 5/src/main/java/com/vit/result/ResultApplication.java
@@ -0,0 +1,16 @@
+package com.vit.result;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main Entry Point for the VIT Semester Result Application.
+ * Powered by Spring Boot 3 & Spring MVC.
+ */
+@SpringBootApplication
+public class ResultApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResultApplication.class, args);
+    }
+}
